d4ca1aa42b4bb1fe4fc2e79f55e88cf2cf5b5f47
akshatchawla1997 <akshatchawla1997@gmail.com>
2026-07-05 02:23:01 +0530

    removed the unused code from the clases

--- strings/ReverseString.py
+++ strings/ReverseString.py
@@ -8,15 +8,7 @@ class String:
     def lengthOfLastWord(self) -> int:
         str = self.string.strip().split(" ")
         print(len(str[len(str)-1]))
-        # i = str[len(str)-1]
-        # count = 0 
-        # for i in self.string[::-1]:
-        #     if(i != " "):
-        #         count = (len(str[len(str)-1]))
-        #         print(f"count is {count}")  
-        # return count
-        
-            
+           
 
 # s = String("Hello, World!")
 # print(s.reverse())  # Output: !dlroW ,olleH
