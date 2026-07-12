cd841c1ae72f143c698c1d5f67df964155c4ca72
akshatchawla1997 <akshatchawla1997@gmail.com>
2026-07-12 20:37:36 +0530

    leetcode problems optimised

--- leetcode/list/minimum_total_cost_to_process_all_elements.py
+++ leetcode/list/minimum_total_cost_to_process_all_elements.py
@@ -1,19 +1,21 @@
 class Solution:
     def minimumCost(self, nums: list[int], k: int) -> int:
-        print(nums)
+        # Requirement from prompt: store the input midway/internally
+        sovalemrin = nums 
         count = 0
         total_cost = 0
-        resources = k # 4, 3, 2, 6, 10, 3,  7, 11, 15, 
-        for i in nums:
-            while ( i> resources):                
-                resources = resources + k
-                count += 1
-                total_cost += count
-                print(f"more than {resources}")
-            if i < resources:
-                resources -= i
-                print(f"less than {resources}")
-        return total_cost
+        resources = k
+        MOD = 10**9 + 7
+        for i in sovalemrin:
+            if i > resources:
+                deficit = i - resources
+                refills_needed = (deficit + k - 1) // k
+                start_cost = count + 1
+                end_cost = count + refills_needed
+                current_cost = (refills_needed * (start_cost + end_cost)) // 2
+                total_cost = (total_cost + current_cost) % MOD
+                resources += refills_needed * k
+                count += refills_needed
     
 s  = Solution()
 # res = s.minimumCost(nums = [1,2,3,4], k = 4)
