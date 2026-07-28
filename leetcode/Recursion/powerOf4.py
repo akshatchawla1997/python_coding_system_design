d1863e49f2265e27e92be9c9ff1c3822701917fc
akshatchawla1997 <akshatchawla1997@gmail.com>
2026-07-28 07:05:47 +0530

    more questions as power of 3 added

---
+++ leetcode/Recursion/powerOf4.py
@@ -0,0 +1,10 @@
+def powerOf4(n:int)->bool:
+    if n < 0:
+        return False
+    if n == 1:
+        return True
+    if n%4 != 0:
+        return False
+    return powerOf4(n // 4)
+
+print(powerOf4(4))
